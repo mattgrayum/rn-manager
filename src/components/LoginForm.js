126cf8eec0f46b50e46fd8f2fcbfeea17c2acab5
Grayum <MGrayum@abc.ca.gov>
2019-05-16 12:45:37 -0700

    Added login form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,27 @@
 import React from 'react'
-import { Card } from './common'
+import { Card, CardSection, Input, Button } from './common'
 
 class LoginForm extends React.Component {
     render() {
         return (
-            <Card />
+            <Card>
+                <CardSection>
+                    <Input
+                        label="Email"
+                        placeholder="email@gmail.com"
+                    />
+                </CardSection>
+                <CardSection>
+                    <Input
+                        secureTextEntry
+                        label="Password"
+                        placeholder="Secure password"
+                    />
+                </CardSection>
+                <CardSection>
+                    <Button buttonText="Login" />
+                </CardSection>
+            </Card>
         )
     }
 }
