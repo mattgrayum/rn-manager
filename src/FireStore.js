364780d7a898b65368d784551c51fc242a4bbf49
Grayum <MGrayum@abc.ca.gov>
2019-05-21 10:40:47 -0700

    working on moving firestore to own file

--- src/FireStore.js
+++ src/FireStore.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase'
-import 'firebase/firestore'
+import '@firebase/firestore'
 
 const FireStore = () => {
 
@@ -11,62 +11,72 @@ const FireStore = () => {
         storageBucket: 'manager-9522b.appspot.com',
         messagingSenderId: '830271348290',
         appId: '1:830271348290:web:1f6ed9f2c09e292a'
-      }
+    }
 
-    if(!firebase.apps.length){
+    if (!firebase.apps.length) {
         firebase.initializeApp(config)
     }
 
     const db = firebase.firestore()
+    const fieldValue = firebase.firestore.FieldValue
+    const currentUser = firebase.auth().currentUser
 
-    const signIn = (email, password) => 
+    // PUBLIC - sign a user in
+    const signIn = (email, password) =>
         firebase.auth().signInWithEmailAndPassword(email, password)
 
-    const createUser = (email, password) => 
+    // PUBLIC - create a new user
+    const createUser = (email, password) =>
         firebase.auth().createUserWithEmailAndPassword(email, password)
 
-    const getCurrentUser = () => firebase.auth().currentUser
+    // PUBLIC - add an employee to a user's profile
+    const addEmployee = (name, phone, shift) => {
 
-    const userId = getCurrentUser().uid
+        if (currentUser) {
 
-    const getUserRef = (db, userId) => db.collection('users').doc(userId)
-    
-    const userRef = getUserRef(db, userId)
+            const userRef = db.collection('users').doc(currentUser.uid)
 
-    const employees = {
-        employees: firebase.firestore.FieldValue.arrayUnion({ name, phone, shift })
+            console.log({ employees: firebase.firestore().FieldValue })
+
+            // Create an object to pass to the update() method to add an 
+            // employee to the stored array of employees
+            const employees = {
+                employees: fieldValue.arrayUnion({ name, phone, shift })
+            }
+
+            // Create an object to pass to the set() method that adds an email
+            // property to a new user that is adding their first employee.
+            // TODO: Set up new user profile when they first create their user
+            //          account
+            const newUserEmployee = {
+                ...employees,
+                email: currentUser.email
+            }
+
+            // Check to see if the user has a profile yet, and then take the
+            // appropriate action.
+            userRef.get()
+                .then(doc => {
+                    if (doc.exists) {
+                        return userRef.update(employees)
+                    }
+                    else {
+                        return userRef.set(newUserEmployee)
+                    }
+                })
+                .catch(error => console.log('checkUserExists: ', error))
+        }
     }
-
-    const newUserEmployee = {
-        ...employees,
-        email: firebase.auth().currentUser.email
-    }
-
-    const addEmployee = () => {
-        userRef.get()
-            .then(doc => {
-                if (doc.exists) {
-                    userRef.update(employees)
-                }
-                else {
-                    userRef.set(newUserEmployee)
-                }
-            })
-            .catch(error => console.log(error))
-    }
-
-        
 
 
 
     return {
-        db, 
         signIn,
-        createUser
+        createUser,
+        addEmployee
     }
 }
 
 export default FireStore
 
 
-  
--- src/actions/EmployeeCreateActions.js
+++ src/actions/EmployeeCreateActions.js
@@ -1,5 +1,3 @@
-import firebase from 'firebase'
-import 'firebase/firestore'
 import FireStore from '../FireStore'
 import { EMPLOYEE_ADDED } from './types'
 
@@ -8,57 +6,13 @@ import { EMPLOYEE_ADDED } from './types'
 export const addEmployee = (name, phone, shift) => {
 
     return dispatch => {
-
-        const db = firebase.firestore()
-        const id = firebase.auth().currentUser.uid
-        const userRef = getUserRef(db, id)
-        const newEmployee = { name, phone, shift }
-        const employees = {
-            employees: firebase.firestore.FieldValue.arrayUnion({ name, phone, shift })
-        }
-        const newUser = {
-            ...employees,
-            email: firebase.auth().currentUser.email
-        }
-
-        userRef.get()
-            .then(doc => {
-                if (doc.exists) {
-                    userRef.update(employees)
-                }
-                else {
-                    userRef.set(newUser)
-                }
+        FireStore().addEmployee(name, phone, shift)
+            .then(() => {
+                dispatch({
+                    type: EMPLOYEE_ADDED,
+                    payload: { name, phone, shift }
+                })
             })
-            .catch(error => console.log(error))
+            .catch(error => console.log('addEmployee: ', error))
     }
-}
-
-const getUserRef = (db, userId) => {
-    return db.collection('users').doc(userId)
-}
-
-const docExists = (ref) => {
-
-    ref.get()
-        .then(doc => {
-            console.log(doc)
-            if (doc.exists) {
-                return true
-            }
-            return false
-        })
-        .catch(error => console.log(error))
-}
-
-const updateEmployees = (dispatch, userRef, newEmployee) => {
-    userRef.set({ employee: newEmployee })
-        .then(data => {
-            console.log('data: ', data)
-            dispatch({
-                type: EMPLOYEE_ADDED,
-                payload: newEmployee
-            })
-        })
-        .catch(error => console.log('error: ', error))
 }
